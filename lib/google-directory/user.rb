e66c87f51ac79176904aae18661574a0e5e10b90
Ben Matheja <post@benmatheja.de>
2016-02-25 15:19:09 +0100

    removed unused methods

--- lib/google-directory/user.rb
+++ lib/google-directory/user.rb
@@ -3,15 +3,7 @@ require 'json'
 
 module GoogleDirectory
   class User
-
-    #private_class_method :new
-
-    def self.fetch
-      new.tap do |instance|
-        instance.users
-      end
-    end
-
+    
     def api
       GoogleDirectory::Api.instance
     end
