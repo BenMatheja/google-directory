cd338a58a5586931a3201b62648f0fc42c375ab9
Ben Matheja <post@benmatheja.de>
2016-03-18 11:06:30 +0100

    added method to query users by group, receiving their entire contact information

--- lib/google-directory/user.rb
+++ lib/google-directory/user.rb
@@ -11,7 +11,22 @@ module GoogleDirectory
     def users
       api.users.users
     end
- 
+
+    def groups
+	   api.groups
+    end
+
+    def members(id)
+      members = api.members(id).members
+      users = users()
+      users.each do |u|
+        if ! members.include?(u['id'])
+          users.delete(u)
+        end
+      end
+      users
+    end
+
   end
 end
 
